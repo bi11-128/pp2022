09c07499b43f9a0822e342c8d4f5a9f1fef0d8ba
bi11-128 <kienvd.bi11-128@st.usth.edu.vn>
2022-04-06 07:01:28 +0700

    lab3python

--- 3.student.mark.oop.math.py
+++ 3.student.mark.oop.math.py
@@ -1,12 +1,25 @@
+# nhập vào số tín chỉ của từng môn
+# tính tổng số điểm của từng môn học
+# tính GPA 
+import curses 
+from curses import wrapper
+import numpy as np
 import math as mat
+import time
+
+
 student=[]
 course=[]
+numcredit=[]
 stumark={}
 name0=str(0)
+sumcredit=0
+sortGPA={}
 class CourseInfo:
   def __init__(self):
     self.__name=0
     self.__ID=0
+    self.__credit=0
   def get__name(self):
     return self.__name
   def set__name(self,t):
@@ -15,8 +28,12 @@ class CourseInfo:
     return self.__ID
   def set__ID(self,i):
     self.__ID=i  
+  def get__credit(self):
+    return self.__credit  
+  def set__credit(self,c):
+    self.__credit=c  
   def inp(self):
-    return f"Name:{self.__name}|ID:{self.__ID}"
+    return f"Name:{self.__name}|ID:{self.__ID}|Credit:{self.__credit}"
 class StudentMark(CourseInfo):             
   def __init__(self,s,m):
     CourseInfo.__init__(self)
@@ -79,15 +96,19 @@ def Courseinfo():
   while(1):
     names=input("Enter COURSE NAME:")
     if names != name0:
-       id=input("Enter course id:") 
+       id=input("Enter course id:")
+       cre=input("Enter course number of credit")
+       numcredit.append(cre)
        cour=CourseInfo()
        cour.set__ID(id)
        cour.set__name(names)
+       cour.set__credit(cre)
        l=cour.inp()
        course.append(l)
     else :
       course.append(names) 
       break 
+  sumcredit=np.sum(numcredit)  
 
 # List courses
 def Courselist():
@@ -103,24 +124,17 @@ def Courselist():
 
 #Select a course, input marks for student in this course   
 def inStudentmark():
-    s = input("Input the subject:")                                       
-    mark=[]
-    stumark[s]=mark
-    while(1):
-      names=input("Enter STUDENT NAME:")
-      if names != name0: 
-        id=input("Enter student id:")
-        m=input(f"{names}'s mark in {s} is:")
-        f=float(m)
-        stu=StudentMark(s,mat.floor(f))   #use floor() function
-        stu.set__ID(id)
-        stu.set__name(names)
-        l=stu.inp()
-        mark.append(l)
+  print("Enter 0 for COURSE NAME if you want to stop inputting\n")
+  while(1):
+    n=input("ENTER STUDENT NAME:")
+    if n != name0:
+      l=[]
+      stumark[n]=l
+      for i in range(len(course)):
+        m=input("{n} mark for {course[i]} is:")
+        l.append(m)
       else:
         break  
-
-
 
 #Show student marks for a given course
 def ListStudentmark():
@@ -131,7 +145,34 @@ def ListStudentmark():
       print(stumark[s][i]) 
   else:
       print("Subject not found,please use Studentmark() to add new course")
-    
+
+
+
+def GPAcount(n):
+  print("Enter 0 for STUDENT NAME TO END TO LOOP")
+  while(1):  
+   n=input("Enter STUDENT NAME:")
+   if n!=name0:
+    if n in stumark:
+     t=np.multiply(stumark[n],numcredit)
+     m=sumcredit
+     GPA=round(t/m,2)
+     return GPA
+    else:
+     print("NO student name {n},Please Enter STUDENT NAME again")
+   else:
+     break  
+
+
+
+def sortStudentList():
+  for n in stumark:
+    aGPA=GPAcount(n)
+    sortGPA[n]=aGPA
+  finalsortGPA=sorted(sortGPA.items(),key=lambda x:x[1],reverse=True)
+  for i in range(finalsortGPA):
+    print(finalsortGPA[i])
+
 
 ##Studentinfo()
 #Studentlist()
@@ -139,3 +180,15 @@ def ListStudentmark():
 #Courselist()
 inStudentmark()
 ListStudentmark()
+
+def main(stdscr):
+  stdscr.addstr("Input a student name")
+  key=stdscr.getstr(0,0,4)
+  GPAm=GPAcount(key)
+  stdscr.addstr(f"key's GPA:{GPAm}")
+  stdscr.refresh()
+  stdscr.getch()
+
+wrapper(main)
+
+
