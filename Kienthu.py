ea711e067042bb52253f3deba742b53aa0a7382f
bi11-128 <kienvd.bi11-128@st.usth.edu.vn>
2022-04-08 14:36:59 +0700

    newpush

---
+++ Kienthu.py
@@ -0,0 +1,14 @@
+
+# How do computer store sound
+# https://null-byte.wonderhowto.com/how-to/steganography-hide-secret-data-inside-image-audio-file-seconds-0180936/
+#AES Rijndael
+#tra trên youtube:audio steganography in python
+# tra xem các ứng dụng chuyển đổi chữ sang âm hoạt động như thế nào dùng algo gì
+# Tra 
+y=[10,25,4,8,5,9,1]
+for i in range(len(y)):
+    if (y[i]==5):
+        print(i)
+        break
+
+
